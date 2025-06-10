7d7060d7a6707bfb433e208219ea2391d3da703d
Charles <charleschuez@utp.ac.pa>
2025-06-10 15:38:02 -0500

    pequeños cambios

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+function AboutPage() {
+  return (
+	<div className="text-center p-10">
+		<h1 className="text-2xl font-bold my-4">
+			About
+		</h1>
+		<p className="text-gray-300">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, aperiam non quae laudantium voluptatum eligendi tenetur dolorem, ullam sunt dolorum perspiciatis sapiente veniam nam sequi et ab natus vitae itaque accusantium suscipit doloremque placeat? Molestias minus dolor fuga provident magnam beatae dolorem consequuntur vel, vitae in quia ipsam saepe? Animi earum reiciendis dicta deserunt dignissimos eos sunt, nulla facere ea harum quia asperiores quos hic maiores quo id nisi consequatur. Eius illum ipsum excepturi non laboriosam neque nostrum ut deleniti! Qui, sed. Esse, repudiandae animi dignissimos placeat eaque quae officiis totam saepe a nam repellat itaque cumque obcaecati ullam illum natus aliquid pariatur! Consectetur eveniet cumque veritatis saepe voluptas soluta delectus at. Illum aut quos deserunt aperiam nisi corporis assumenda, optio alias sit? Vero, velit dolorum. Illo reprehenderit provident recusandae suscipit mollitia saepe esse doloribus modi eius magni aspernatur harum ratione iusto incidunt, accusamus explicabo nesciunt. Nihil deleniti nemo explicabo eius corrupti repudiandae natus quibusdam, aliquid non tenetur modi quae fuga laboriosam sequi sit libero dolores, obcaecati maxime nostrum repellendus facere atque aperiam pariatur! Nostrum, aut! Illum consequatur, voluptatum debitis in cumque excepturi, facere eos rem esse magnam id autem dolore delectus quaerat doloribus modi. Laudantium consequatur ex ducimus incidunt ipsa suscipit reprehenderit exercitationem qui voluptatem voluptatum, non harum iusto provident illo ut animi officiis aut aspernatur dignissimos repudiandae. Repudiandae, perspiciatis magnam. Fugiat necessitatibus, repellat commodi doloribus optio sit dolor omnis dolorem odit voluptas corporis voluptatum officia laborum in pariatur quibusdam quaerat quasi provident odio porro vero quae? Suscipit mollitia dolore nesciunt aspernatur inventore. Alias, odio! Saepe cupiditate necessitatibus quidem eligendi dolorem ex debitis molestias. Velit deleniti necessitatibus dicta placeat numquam asperiores mollitia, beatae amet ratione assumenda rerum, est incidunt ab recusandae, quibusdam accusantium nostrum facilis. Officiis iusto non molestias unde dolor itaque at necessitatibus ex facilis dignissimos! Possimus, excepturi!
+		</p>
+	</div>
+  )
+}
+
+export default AboutPage
